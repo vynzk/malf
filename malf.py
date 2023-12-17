a0b5aa904e9de8c6bd1459da662fc87e3f3e607f
vynzk <vyanez20@alumnos.utalca.cl>
2023-12-17 10:46:27 -0300

    funciona malf.py y corre el parser, falta ejecutar el codigo

--- malf.py
+++ malf.py
@@ -1,188 +1,176 @@
 import ply.lex as lex
 import ply.yacc as yacc
+global variables
 variables = {}
-# Definición de tokens
-tokens = (
-    'read',
-    'write',
-    'if',
-    'else',
-    'then',
-    'endif',
-    'while',
-    'do',
-    'wend',
-    'ID',
-    'NUM',
-    'BOOL',
-    'OPERATOR',
-    'SEMICOLON',
-    'ENTER',
-    'parentesis_izq',
-    'parentesis_der',
-    'EQUALS'
-)
+global errores
+errores = 0
+def gramatica(codigo):
+    # Definición de tokens
+    tokens = (
+        'read',
+        'write',
+        'if',
+        'else',
+        'then',
+        'endif',
+        'while',
+        'do',
+        'wend',
+        'ID',
+        'NUM',
+        'BOOL',
+        'OPERATOR',
+        'SEMICOLON',
+        'ENTER',
+        'parentesis_izq',
+        'parentesis_der',
+        'EQUALS'
+    )
 
-# Expresiones regulares para tokens simples
-t_SEMICOLON = r';'
-t_ignore = ' \t'
-t_parentesis_izq = r'\('
-t_parentesis_der = r'\)'
-t_ENTER = r'\n'
-t_EQUALS = r'='  # Expresión regular para la asignación
-# tokens simples
-t_read = r'read'
-t_write = r'write'
-t_if = r'if'
-t_then = r'then'
-t_endif = r'endif'
-t_while = r'while'
-t_do = r'do'
-t_wend = r'wend'
-t_else = r'else'
-# Reglas para tokens más complejos
-def t_BOOL(t):
-    r'true|false'
-    t.value = True if t.value == 'true' else False
-    return t
+    # Expresiones regulares para tokens simples
+    t_SEMICOLON = r';'
+    t_ignore = ' \t'
+    t_parentesis_izq = r'\('
+    t_parentesis_der = r'\)'
+    t_ENTER = r'\n'
+    t_EQUALS = r'='  # Expresión regular para la asignación
+    # tokens simples
+    t_read = r'read'
+    t_write = r'write'
+    t_if = r'if'
+    t_then = r'then'
+    t_endif = r'endif'
+    t_while = r'while'
+    t_do = r'do'
+    t_wend = r'wend'
+    t_else = r'else'
+    # Reglas para tokens más complejos
+    def t_BOOL(t):
+        r'true|false'
+        t.value = True if t.value == 'true' else False
+        return t
 
-def t_ID(t):
-    r'\$[a-zA-Z_][a-zA-Z_0-9]*'
-    return t
+    def t_ID(t):
+        r'\$[a-zA-Z_][a-zA-Z_0-9]*'
+        return t
 
-def t_NUM(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+    def t_NUM(t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
 
-def t_OPERATOR(t):
-    r'<|<=|>|>=|==|!=|\+|\-|\*|\/|\%'
-    return t
+    def t_OPERATOR(t):
+        r'\+|\-|\*|\/|<=|>=|<|>|==|!='
+        return t
 
-# Definición de errores léxicos
-def t_error(t):
-    print(f"Error léxico: Carácter inesperado '{t.value[0]}'")
-    t.lexer.skip(1)
+    # Definición de errores léxicos
+    def t_error(t):
+        print(f"Error léxico: Carácter inesperado '{t.value[0]}'")
+        global errores
+        errores += 1
+        t.lexer.skip(1)
 
-# Definición de reglas sintácticas
-def p_statement_list(p):
-    '''
-    statement_list : statement_list statement ENTER
-                   | statement ENTER
-                   | statement_list statement
-                   | statement
-    '''
-    pass
+    # Definición de reglas sintácticas
+    def p_statement_list(p):
+        '''
+        statement_list : statement_list statement ENTER
+                    | statement ENTER
+                    | statement_list statement
+                    | statement
+        '''
+        pass
 
-def p_statement(p):
-    '''
-    statement : read_statement
-              | if_statement
-              | while_statement
-              | write_statement
-              | assignment_statement
-    '''
-    pass
+    def p_statement(p):
+        '''
+        statement : read_statement
+                | if_statement
+                | while_statement
+                | write_statement
+                | assignment_statement
+        '''
+        pass
 
-def p_assignment_statement(p):
-    'assignment_statement : ID EQUALS expression SEMICOLON'
-    variables[p[1]] = p[3]
-    p[0] = f"Assignment to : {p[1]} = {p[3]}"
-    print(p[0])
+    def p_assignment_statement(p):
+        'assignment_statement : ID EQUALS expression SEMICOLON'
+        pass
 
-def p_read_statement(p):
-    'read_statement : read ID SEMICOLON'
-    variable_name = p[2]
-    user_input = input(f"Enter value for {variable_name}: ")
-    try:
-        variables[variable_name] = int(user_input)
-        print(f"{variable_name} set to {user_input}")
-    except ValueError:
-        print(f"Error: {user_input} is not a valid integer.")
+    def p_read_statement(p):
+        'read_statement : read ID SEMICOLON'
+        pass
 
-def p_if_statement(p):
-    '''
-    if_statement : if parentesis_izq condition parentesis_der then ENTER statement_list endif SEMICOLON
-                | if parentesis_izq condition parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON
-                | if parentesis_izq operando_statement parentesis_der then ENTER statement_list endif SEMICOLON
-                | if parentesis_izq operando_statement parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON
-    '''
-    pass
+    def p_if_statement(p):
+        '''
+        if_statement : if parentesis_izq condition parentesis_der then ENTER statement_list endif SEMICOLON
+                    | if parentesis_izq condition parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON
+                    | if parentesis_izq operando_statement parentesis_der then ENTER statement_list endif SEMICOLON
+                    | if parentesis_izq operando_statement parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON
+        '''
+        pass
 
-def p_condition(p):
-    'condition : BOOL'
-    print(f"Condition: {p[1]}")
+    def p_condition(p):
+        'condition : BOOL'
+        pass
 
-def p_while_statement(p):
-    '''
-    while_statement : while parentesis_izq condition parentesis_der do ENTER statement_list wend SEMICOLON
-                    | while parentesis_izq operando_statement parentesis_der do ENTER statement_list wend SEMICOLON
-    '''
-    pass
+    def p_while_statement(p):
+        '''
+        while_statement : while parentesis_izq condition parentesis_der do ENTER statement_list wend SEMICOLON
+                        | while parentesis_izq operando_statement parentesis_der do ENTER statement_list wend SEMICOLON
+        '''
+        pass
 
-def p_write_statement(p):
-    'write_statement : write expression SEMICOLON'
-    print(p[2])
+    def p_write_statement(p):
+        'write_statement : write expression SEMICOLON'
 
-def p_expression(p):
-    '''
-    expression : ID
-               | NUM
-               | operando_statement
-    '''
-    p[0] = p[1]
-    pass
+    def p_expression(p):
+        '''
+        expression : ID
+                | NUM
+                | operando_statement
+        '''
+        pass
 
-def p_operando_statement(p):
-    '''
-    operando_statement : expression OPERATOR expression
-                        | expression OPERATOR ID    
-    '''
-    if p[2] == '+':
-        p[0] = p[1] + p[3]
-    elif p[2] == '-':
-        p[0] = p[1] - p[3]
-    elif p[2] == '*':
-        p[0] = p[1] * p[3]
-    elif p[2] == '/':
-        if p[3] != 0:
-            p[0] = p[1] / p[3]
+    def p_operando_statement(p):
+        '''
+        operando_statement : expression OPERATOR expression
+                            | expression OPERATOR ID    
+        '''
+        pass
+
+
+    # Definición de errores sintácticos
+    def p_error(p):
+        global errores
+        if p:
+            print(f"Error sintáctico: Token inesperado '{p.value}' en la línea {p.lineno}")
+            errores += 1
         else:
-            print("Error: Division by zero")
-            p[0] = None
-    elif p[2] == '%':
-        if isinstance(p[1], int) and isinstance(p[3], int):
-            p[0] = p[1] % p[3]
-        else:
-            print("Error: Modulo operation requires integer operands")
-            p[0] = None
-    elif p[2] == '<':
-        p[0] = p[1] < p[3]
-    elif p[2] == '<=':
-        p[0] = p[1] <= p[3]
-    elif p[2] == '>':
-        p[0] = p[1] > p[3]
-    elif p[2] == '>=':
-        p[0] = p[1] >= p[3]
-    elif p[2] == '==':
-        p[0] = p[1] == p[3]
-    elif p[2] == '!=':
-        p[0] = p[1] != p[3]
+            print("Error sintáctico: Fin de archivo inesperado")
+            errores += 1
 
+    # Construcción del analizador léxico
+    lexer = lex.lex()
+    parser = yacc.yacc()
+    lexer = lex.lex()
+    parser.parse(codigo, lexer=lexer)
+    return 
 
-# Definición de errores sintácticos
-def p_error(p):
-    if p:
-        print(f"Error sintáctico: Token inesperado '{p.value}' en la línea {p.lineno}")
-    else:
-        print("Error sintáctico: Fin de archivo inesperado")
+# ejecutar codigo
+def ejecutar(codigo):
+    
+    
 
-# Construcción del analizador léxico
-lexer = lex.lex()
-parser = yacc.yacc()
-lexer = lex.lex()
+codigo = """write 1;
+read $a;
+if ($a != $b) then
+    write 1;
+    $a = 1;
+    $b = 2;
+    $a = $a + $b;
+else
+    write 0;
+endif;"""
 
-codigo = """write 1;"""
+if (errores == 0):
+    print("---- El código es correcto ----")
+    ejecutar(codigo)
 
-result = parser.parse(codigo, lexer=lexer)
-print("Variables después de la ejecución:", variables)
--- parsetab.py
+++ parsetab.py
@@ -6,7 +6,7 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'BOOL ENTER EQUALS ID NUM OPERATOR SEMICOLON do else endif if parentesis_der parentesis_izq read then wend while write\n    statement_list : statement_list statement ENTER\n                   | statement ENTER\n                   | statement_list statement\n                   | statement\n    \n    statement : read_statement\n              | if_statement\n              | while_statement\n              | write_statement\n              | assignment_statement\n    assignment_statement : ID EQUALS expression SEMICOLONread_statement : read ID SEMICOLON\n    if_statement : if parentesis_izq condition parentesis_der then ENTER statement_list endif SEMICOLON\n                | if parentesis_izq condition parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON\n                | if parentesis_izq operando_statement parentesis_der then ENTER statement_list endif SEMICOLON\n                | if parentesis_izq operando_statement parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON\n    condition : BOOL\n    while_statement : while parentesis_izq condition parentesis_der do ENTER statement_list wend SEMICOLON\n                    | while parentesis_izq operando_statement parentesis_der do ENTER statement_list wend SEMICOLON\n    write_statement : write expression SEMICOLON\n    expression : ID\n               | NUM\n               | operando_statement\n    \n    operando_statement : expression OPERATOR expression\n                        | expression OPERATOR ID    \n    '
+_lr_signature = 'BOOL ENTER EQUALS ID NUM OPERATOR SEMICOLON do else endif if parentesis_der parentesis_izq read then wend while write\n        statement_list : statement_list statement ENTER\n                    | statement ENTER\n                    | statement_list statement\n                    | statement\n        \n        statement : read_statement\n                | if_statement\n                | while_statement\n                | write_statement\n                | assignment_statement\n        assignment_statement : ID EQUALS expression SEMICOLONread_statement : read ID SEMICOLON\n        if_statement : if parentesis_izq condition parentesis_der then ENTER statement_list endif SEMICOLON\n                    | if parentesis_izq condition parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON\n                    | if parentesis_izq operando_statement parentesis_der then ENTER statement_list endif SEMICOLON\n                    | if parentesis_izq operando_statement parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON\n        condition : BOOL\n        while_statement : while parentesis_izq condition parentesis_der do ENTER statement_list wend SEMICOLON\n                        | while parentesis_izq operando_statement parentesis_der do ENTER statement_list wend SEMICOLON\n        write_statement : write expression SEMICOLON\n        expression : ID\n                | NUM\n                | operando_statement\n        \n        operando_statement : expression OPERATOR expression\n                            | expression OPERATOR ID    \n        '
     
 _lr_action_items = {'read':([0,1,2,3,4,5,6,7,13,14,23,24,32,34,45,46,47,48,49,50,51,52,59,60,61,62,63,64,65,66,69,70,],[8,8,-4,-5,-6,-7,-8,-9,-3,-2,-1,-11,-19,-10,8,8,8,8,8,8,8,8,-12,8,-14,8,-17,-18,8,8,-13,-15,]),'if':([0,1,2,3,4,5,6,7,13,14,23,24,32,34,45,46,47,48,49,50,51,52,59,60,61,62,63,64,65,66,69,70,],[10,10,-4,-5,-6,-7,-8,-9,-3,-2,-1,-11,-19,-10,10,10,10,10,10,10,10,10,-12,10,-14,10,-17,-18,10,10,-13,-15,]),'while':([0,1,2,3,4,5,6,7,13,14,23,24,32,34,45,46,47,48,49,50,51,52,59,60,61,62,63,64,65,66,69,70,],[11,11,-4,-5,-6,-7,-8,-9,-3,-2,-1,-11,-19,-10,11,11,11,11,11,11,11,11,-12,11,-14,11,-17,-18,11,11,-13,-15,]),'write':([0,1,2,3,4,5,6,7,13,14,23,24,32,34,45,46,47,48,49,50,51,52,59,60,61,62,63,64,65,66,69,70,],[12,12,-4,-5,-6,-7,-8,-9,-3,-2,-1,-11,-19,-10,12,12,12,12,12,12,12,12,-12,12,-14,12,-17,-18,12,12,-13,-15,]),'ID':([0,1,2,3,4,5,6,7,8,12,13,14,16,17,18,23,24,32,33,34,45,46,47,48,49,50,51,52,59,60,61,62,63,64,65,66,69,70,],[9,9,-4,-5,-6,-7,-8,-9,15,20,-3,-2,20,20,20,-1,-11,-19,40,-10,9,9,9,9,9,9,9,9,-12,9,-14,9,-17,-18,9,9,-13,-15,]),'$end':([1,2,3,4,5,6,7,13,14,23,24,32,34,59,61,63,64,69,70,],[0,-4,-5,-6,-7,-8,-9,-3,-2,-1,-11,-19,-10,-12,-14,-17,-18,-13,-15,]),'ENTER':([2,3,4,5,6,7,13,24,32,34,41,42,43,44,54,56,59,61,63,64,69,70,],[14,-5,-6,-7,-8,-9,23,-11,-19,-10,45,46,47,48,60,62,-12,-14,-17,-18,-13,-15,]),'endif':([2,3,4,5,6,7,13,14,23,24,32,34,49,50,59,61,63,64,65,66,69,70,],[-4,-5,-6,-7,-8,-9,-3,-2,-1,-11,-19,-10,53,55,-12,-14,-17,-18,67,68,-13,-15,]),'else':([2,3,4,5,6,7,13,14,23,24,32,34,49,50,59,61,63,64,69,70,],[-4,-5,-6,-7,-8,-9,-3,-2,-1,-11,-19,-10,54,56,-12,-14,-17,-18,-13,-15,]),'wend':([2,3,4,5,6,7,13,14,23,24,32,34,51,52,59,61,63,64,69,70,],[-4,-5,-6,-7,-8,-9,-3,-2,-1,-11,-19,-10,57,58,-12,-14,-17,-18,-13,-15,]),'EQUALS':([9,],[16,]),'parentesis_izq':([10,11,],[17,18,]),'NUM':([12,16,17,18,33,],[21,21,21,21,21,]),'SEMICOLON':([15,19,20,21,22,25,39,40,53,55,57,58,67,68,],[24,32,-20,-21,-22,34,-23,-20,59,61,63,64,69,70,]),'BOOL':([17,18,],[28,28,]),'OPERATOR':([19,20,21,22,25,27,29,31,39,40,],[33,-20,-21,-22,33,-22,33,-22,33,-20,]),'parentesis_der':([21,22,26,27,28,30,31,39,40,],[-21,-22,35,36,-16,37,38,-23,-20,]),'then':([35,36,],[41,42,]),'do':([37,38,],[43,44,]),}
 
@@ -27,28 +27,28 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> statement_list","S'",1,None,None,None),
-  ('statement_list -> statement_list statement ENTER','statement_list',3,'p_statement_list','malf.py',70),
-  ('statement_list -> statement ENTER','statement_list',2,'p_statement_list','malf.py',71),
-  ('statement_list -> statement_list statement','statement_list',2,'p_statement_list','malf.py',72),
-  ('statement_list -> statement','statement_list',1,'p_statement_list','malf.py',73),
-  ('statement -> read_statement','statement',1,'p_statement','malf.py',79),
-  ('statement -> if_statement','statement',1,'p_statement','malf.py',80),
-  ('statement -> while_statement','statement',1,'p_statement','malf.py',81),
-  ('statement -> write_statement','statement',1,'p_statement','malf.py',82),
-  ('statement -> assignment_statement','statement',1,'p_statement','malf.py',83),
-  ('assignment_statement -> ID EQUALS expression SEMICOLON','assignment_statement',4,'p_assignment_statement','malf.py',88),
-  ('read_statement -> read ID SEMICOLON','read_statement',3,'p_read_statement','malf.py',94),
-  ('if_statement -> if parentesis_izq condition parentesis_der then ENTER statement_list endif SEMICOLON','if_statement',9,'p_if_statement','malf.py',105),
-  ('if_statement -> if parentesis_izq condition parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON','if_statement',12,'p_if_statement','malf.py',106),
-  ('if_statement -> if parentesis_izq operando_statement parentesis_der then ENTER statement_list endif SEMICOLON','if_statement',9,'p_if_statement','malf.py',107),
-  ('if_statement -> if parentesis_izq operando_statement parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON','if_statement',12,'p_if_statement','malf.py',108),
-  ('condition -> BOOL','condition',1,'p_condition','malf.py',113),
-  ('while_statement -> while parentesis_izq condition parentesis_der do ENTER statement_list wend SEMICOLON','while_statement',9,'p_while_statement','malf.py',118),
-  ('while_statement -> while parentesis_izq operando_statement parentesis_der do ENTER statement_list wend SEMICOLON','while_statement',9,'p_while_statement','malf.py',119),
-  ('write_statement -> write expression SEMICOLON','write_statement',3,'p_write_statement','malf.py',124),
-  ('expression -> ID','expression',1,'p_expression','malf.py',129),
-  ('expression -> NUM','expression',1,'p_expression','malf.py',130),
-  ('expression -> operando_statement','expression',1,'p_expression','malf.py',131),
-  ('operando_statement -> expression OPERATOR expression','operando_statement',3,'p_operando_statement','malf.py',138),
-  ('operando_statement -> expression OPERATOR ID','operando_statement',3,'p_operando_statement','malf.py',139),
+  ('statement_list -> statement_list statement ENTER','statement_list',3,'p_statement_list','malf.py',71),
+  ('statement_list -> statement ENTER','statement_list',2,'p_statement_list','malf.py',72),
+  ('statement_list -> statement_list statement','statement_list',2,'p_statement_list','malf.py',73),
+  ('statement_list -> statement','statement_list',1,'p_statement_list','malf.py',74),
+  ('statement -> read_statement','statement',1,'p_statement','malf.py',80),
+  ('statement -> if_statement','statement',1,'p_statement','malf.py',81),
+  ('statement -> while_statement','statement',1,'p_statement','malf.py',82),
+  ('statement -> write_statement','statement',1,'p_statement','malf.py',83),
+  ('statement -> assignment_statement','statement',1,'p_statement','malf.py',84),
+  ('assignment_statement -> ID EQUALS expression SEMICOLON','assignment_statement',4,'p_assignment_statement','malf.py',89),
+  ('read_statement -> read ID SEMICOLON','read_statement',3,'p_read_statement','malf.py',93),
+  ('if_statement -> if parentesis_izq condition parentesis_der then ENTER statement_list endif SEMICOLON','if_statement',9,'p_if_statement','malf.py',98),
+  ('if_statement -> if parentesis_izq condition parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON','if_statement',12,'p_if_statement','malf.py',99),
+  ('if_statement -> if parentesis_izq operando_statement parentesis_der then ENTER statement_list endif SEMICOLON','if_statement',9,'p_if_statement','malf.py',100),
+  ('if_statement -> if parentesis_izq operando_statement parentesis_der then ENTER statement_list else ENTER statement_list endif SEMICOLON','if_statement',12,'p_if_statement','malf.py',101),
+  ('condition -> BOOL','condition',1,'p_condition','malf.py',106),
+  ('while_statement -> while parentesis_izq condition parentesis_der do ENTER statement_list wend SEMICOLON','while_statement',9,'p_while_statement','malf.py',111),
+  ('while_statement -> while parentesis_izq operando_statement parentesis_der do ENTER statement_list wend SEMICOLON','while_statement',9,'p_while_statement','malf.py',112),
+  ('write_statement -> write expression SEMICOLON','write_statement',3,'p_write_statement','malf.py',117),
+  ('expression -> ID','expression',1,'p_expression','malf.py',121),
+  ('expression -> NUM','expression',1,'p_expression','malf.py',122),
+  ('expression -> operando_statement','expression',1,'p_expression','malf.py',123),
+  ('operando_statement -> expression OPERATOR expression','operando_statement',3,'p_operando_statement','malf.py',129),
+  ('operando_statement -> expression OPERATOR ID','operando_statement',3,'p_operando_statement','malf.py',130),
 ]
